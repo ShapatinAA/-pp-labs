9fac12e92ff811713d76cfea87d1c986dc76c30a
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2022-09-09 21:46:16 +0700

    Add files via upload

---
+++ lab0/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "module1.h"
+#include "module2.h"
+#include "module3.h"
+
+int main(int argc, char** argv)
+{
+	using std::cout;
+	cout << "Hello world" << "\n";
+
+	cout << Module1::getMyName() << "\n";
+	cout << Module2::getMyName() << "\n";
+
+	using namespace Module1;
+	cout << getMyName() << "\n";
+	cout << Module2::getMyName() << "\n";
+
+	using namespace Module2;
+	cout << Module2::getMyName() << "\n";
+
+	using Module2::getMyName;
+	cout << getMyName() << "\n";
+
+	cout << Module3::getMyName() << "\n";
+}
